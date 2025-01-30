7b0f8e40163e342438e14bf61bc5d6d197c3b2ed
ptc-saran-jai <58280232+dewsaran@users.noreply.github.com>
2025-01-30 20:14:15 +0700

    update

--- pages/list.vue
+++ pages/list.vue
@@ -24,7 +24,7 @@
             value="btn1"
             :class="{ active: activebtn === 'btn1' }"
             @click="activebtn = 'btn1'"
-            >Pokemon FireRed/LeafGreen {{ user?.first_name }}</v-btn
+            >Pokemon FireRed/LeafGreen @{{ user?.username || 'ไม่มี' }}</v-btn
           >
           <v-btn
             class="btn"
